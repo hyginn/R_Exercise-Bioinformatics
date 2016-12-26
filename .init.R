1288a3e810d7cf13a2f5cc55db30786097b9a45c
hyginn <boris.steipe@utoronto.ca>
2016-12-26 11:52:17 +0800

    source utilities; rm init().

--- .init.R
+++ .init.R
@@ -8,6 +8,10 @@ if (! file.exists("myScript.R")) {
     file.copy("tmp.R", "myScript.R")
 }
 
+source(".utilities.R")
+
 file.edit("R_Exercise-Bioinformatics.R")
+
+rm(init)  # not needed in Workspace after we've source()'d this file
 
 # [End]
